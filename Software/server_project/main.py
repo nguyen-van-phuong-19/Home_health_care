fdf6cb54ba50254f98ee2f16dce844b0e52492d1
nguyen-van-phuong <nguyenvanphuongxiii@gmail.com>
2025-02-21 18:01:18 +0700

    hello

--- Software/server_project/main.py
+++ Software/server_project/main.py
@@ -9,12 +9,27 @@ PASSWORD = "public"
 
 # Callback when connecting to broker
 def on_connect(client, userdata, flags, rc):
+    """
+    Callback when connecting to broker
+    :param client: The client instance for this callback
+    :param userdata: The private user data as set in Client() or userdata_set()
+    :param flags: response flags sent by the broker
+    :param rc: the connection result
+    :return: None
+    """
     print(f"Connected with result code {rc}")
     # Subscribe to topics after connecting
     client.subscribe("test/topic")
 
 # Callback when receiving message
 def on_message(client, userdata, msg):
+    """
+    Callback when receiving message
+    :param client: The client instance for this callback
+    :param userdata: The private user data as set in Client() or userdata_set()
+    :param msg: The message that was received
+    :return: None
+    """
     print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
 
 # Create MQTT client instance
